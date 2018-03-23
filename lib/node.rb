77d47a94e877416ef3a1aba647972701d68da61b
zdcthomas <zdcthomas@yahoo.com>
2018-03-23 14:10:22 -0600

    "Node exists"

---
+++ lib/node.rb
@@ -0,0 +1,9 @@
+class Node
+  def initialize(character = nil)
+    @character = character
+    @children = []
+    @is_end = false
+    @parent = nil
+    @weight = 0
+  end
+end
--- test/node_test.rb
+++ test/node_test.rb
@@ -10,6 +10,5 @@ class NodeTest < MiniTest::Test
   def test_it_exists
     node = Node.new
     assert_instance_of Node, node
-
   end#test exist
 end

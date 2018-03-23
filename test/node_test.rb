a9f72f1072e081a3e2e834edd84222a8b28a4298
zdcthomas <zdcthomas@yahoo.com>
2018-03-23 13:56:55 -0600

    TESTS node exists WIP

---
+++ test/node_test.rb
@@ -0,0 +1,15 @@
+require_relative "../lib/node"
+require 'pry'
+require "Minitest/autorun"
+require "Minitest/pride"
+
+class NodeTest < MiniTest::Test
+  def setup
+  end
+
+  def test_it_exists
+    node = Node.new
+    assert_instance_of Node, node
+
+  end#test exist
+end

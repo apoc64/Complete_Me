9e156e2900b8e902ca694e18c7b23bea873f1b15
apoc64 <sschwedt@gmail.com>
2018-03-24 10:49:47 -0600

    node can be end of word

--- lib/node.rb
+++ lib/node.rb
@@ -1,20 +1,21 @@
 class Node
   attr_reader :children,
               :character,
-              :parent
+              :parent,
+              :is_end
 
 
-  def initialize(character = "", parent = nil) #should default be ""? is_end = false? parent = nil
+  def initialize(character = "", parent = nil, is_end = false) #should default be ""? is_end = false? parent = nil
     @character = character
     @children = []
-    @is_end = false
+    @is_end = is_end
     @parent = parent
     @weight = 0
   end
 
-  def insert(character) #is_end = false?
+  def insert(character, is_end = false) #is_end = false?
     if children_dont_have_character(character)
-      node = Node.new(character, self)
+      node = Node.new(character, self, is_end)
       @children << node
     end
   end

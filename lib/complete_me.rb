5a58f8e8bcb04086fb4083f3b4b92793ad6cf574
zdcthomas <zdcthomas@yahoo.com>
2018-03-25 11:10:17 -0600

    WIP introduces include_word? method

--- lib/complete_me.rb
+++ lib/complete_me.rb
@@ -6,7 +6,17 @@ class CompleteMe
     @root = Node.new
   end
 
-  def include_word?(word)
-  end 
+  # def include_word?(word, node = @root)
+  #   letters = word.split('')
+  #   letters.each do |letter|
+  #     if node.children_dont_have_character(letter)
+  #       return false
+  #     else
+  #       node.children.each do |child_node|
+  #       if include_word?(word, child_node)
+  #
+  #     end
+  # end
+  
 
 end

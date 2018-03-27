5a3458eae6718c337a49a70e21034d398e6d4344
apoc64 <sschwedt@gmail.com>
2018-03-26 20:04:13 -0600

    formatting improvements

--- lib/complete_me.rb
+++ lib/complete_me.rb
@@ -102,12 +102,9 @@ class CompleteMe
   def delete(word)
     node = find(word)
     return nil if node.nil? || !node.is_end
-
     node.is_end = false
     return 0 if node.get_end_nodes.count > 0
-
     delete_parent_nodes(node.parent, node)
-
   end
 
   def delete_parent_nodes(node, child)

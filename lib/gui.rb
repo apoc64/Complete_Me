e4e8f1af6e009cdeb21b17f74bc7567d7be22ad7
zdcthomas <zdcthomas@yahoo.com>
2018-03-27 12:30:50 -0600

    WIP select, dynamically displays suggestions for inputted text

--- lib/gui.rb
+++ lib/gui.rb
@@ -9,33 +9,25 @@ Shoes.app do
 
   stack(margin: 20) do
     para 'Enter a Substring'
+
     flow do
       @user_input = edit_line
       @clear_button = button 'clear'
+      @select_button = button 'select'
       @user_input.change do
-        @results.replace(cm.suggest(@user_input.text)[0..9].join(", "))
-        flow do
-          @possibilities = @results.split(', ')
-          button(@results)
-          @possibilities.each do |possible_word|
-            @selection_buttons = button(possible_word.text)
-          end
-        end
+        @results.replace(cm.suggest(@user_input.text)[0..9].join(', '))
       end
       @clear_button.click do
         @results.replace ""
         @user_input.text = ""
+        hide(@choices)
       end
     end
 
 
     @results = para ''
 
-
-
-
-
-    para 'Import'
+    para 'Import (This may take a few seconds)'
     flow do
       @file_path = edit_line('/usr/share/dict/words')
       @import = button 'Import'
